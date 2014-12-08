e7847dd35e756b1788f789da43c3f1ea9c26df64
dbronsh <dbronsh@users.noreply.github.com>
2014-12-08 12:23:22 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,59 +16,59 @@ PaymentOption.create(
             limit: -1
         },
         {
-            amount: 100.00,
-            amount_display: '$100',
+            amount: 18.00,
+            amount_display: '$18',
             description: '<strong>Package 1: </strong>You receive our print edition',
             shipping_desc: 'add $3 to ship outside the US',
             delivery_desc: 'Estimated delivery: Oct 2013',
-            limit: 250
+            limit: -1
         },
         {
-            amount: 125.00,
-            amount_display: '$125',
+            amount: 36.00,
+            amount_display: '$36',
             description: '<strong>Package 2: </strong>You will receive both our print and digital edition',
             shipping_desc: 'add $3 to ship outside the US',
             delivery_desc: 'Estimated delivery: Oct 2013',
             limit: -1
         },
         {
-            amount: 200.00,
-            amount_display: '$200',
+            amount: 100.00,
+            amount_display: '$100',
             description: '<strong>Package 3: </strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
             shipping_desc: 'add $3 to ship outside the US',
             delivery_desc: 'Estimated delivery: Oct 2013',
             limit: -1
         },
         {
-            amount: 250.00,
-            amount_display: '$250',
-            description: '<strong>Package 4: </strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            shipping_desc: 'add $3 to ship outside the US',
-            delivery_desc: 'Estimated delivery: Oct 2013',
+            amount: 180.00,
+            amount_display: '$180',
+            description: '',
+            shipping_desc: '',
+            delivery_desc: '',
             limit: -1
         },
         {
-            amount: 300.00,
-            amount_display: '$300',
-            description: '<strong>Package 5: </strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            shipping_desc: 'add $3 to ship outside the US',
-            delivery_desc: 'Estimated delivery: Oct 2013',
+            amount: 360.00,
+            amount_display: '$360',
+            description: '',
+            shipping_desc: '',
+            delivery_desc: '',
             limit: -1
         },
         {
             amount: 500.00,
             amount_display: '$500',
-            description: '<strong>Package 6: </strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            shipping_desc: 'add $3 to ship outside the US',
-            delivery_desc: 'Estimated delivery: Oct 2013',
+            description: '',
+            shipping_desc: '',
+            delivery_desc: '',
             limit: -1
         },
         {
             amount: 1000.00,
             amount_display: '$1000',
-            description: '<strong>Package 7: </strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            shipping_desc: 'add $3 to ship outside the US',
-            delivery_desc: 'Estimated delivery: Oct 2013',
+            description: '',
+            shipping_desc: '',
+            delivery_desc: '',
             limit: -1
         }
     ])
