550b543dcec82e2c89f31fc6a241e5f9121a9808
dbronsh <dbronsh@users.noreply.github.com>
2014-12-09 09:21:29 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -16,83 +17,59 @@ PaymentOption.create(
             limit: -1
         },
         {
-            amount: 18.00,
-            amount_display: '$18',
+            amount: 100.00,
+            amount_display: '$100',
             description: '<strong>Package 1: </strong>You receive our print edition',
             shipping_desc: 'add $3 to ship outside the US',
             delivery_desc: 'Estimated delivery: Oct 2013',
-            limit: -1
+            limit: 250
         },
         {
-            amount: 36.00,
-            amount_display: '$36',
+            amount: 125.00,
+            amount_display: '$125',
             description: '<strong>Package 2: </strong>You will receive both our print and digital edition',
             shipping_desc: 'add $3 to ship outside the US',
             delivery_desc: 'Estimated delivery: Oct 2013',
             limit: -1
         },
         {
-            amount: 100.00,
-            amount_display: '$100',
+            amount: 200.00,
+            amount_display: '$200',
             description: '<strong>Package 3: </strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
             shipping_desc: 'add $3 to ship outside the US',
             delivery_desc: 'Estimated delivery: Oct 2013',
             limit: -1
         },
         {
-            amount: 180.00,
-            amount_display: '$180',
-            description: '',
-            shipping_desc: '',
-            delivery_desc: '',
+            amount: 250.00,
+            amount_display: '$250',
+            description: '<strong>Package 4: </strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            shipping_desc: 'add $3 to ship outside the US',
+            delivery_desc: 'Estimated delivery: Oct 2013',
             limit: -1
         },
         {
-            amount: 360.00,
-            amount_display: '$360',
-            description: '',
-            shipping_desc: '',
-            delivery_desc: '',
+            amount: 300.00,
+            amount_display: '$300',
+            description: '<strong>Package 5: </strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            shipping_desc: 'add $3 to ship outside the US',
+            delivery_desc: 'Estimated delivery: Oct 2013',
             limit: -1
         },
         {
             amount: 500.00,
             amount_display: '$500',
-            description: '',
-            shipping_desc: '',
-            delivery_desc: '',
+            description: '<strong>Package 6: </strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            shipping_desc: 'add $3 to ship outside the US',
+            delivery_desc: 'Estimated delivery: Oct 2013',
             limit: -1
         },
         {
             amount: 1000.00,
             amount_display: '$1000',
-            description: '',
-            shipping_desc: '',
-            delivery_desc: '',
+            description: '<strong>Package 7: </strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            shipping_desc: 'add $3 to ship outside the US',
+            delivery_desc: 'Estimated delivery: Oct 2013',
             limit: -1
         }
     ])
-    
-  #  User.create(
-  #  [
-  #      {
-  #          id: '1'
-  #          email: 'aa.aa@sa.sa'
-  #      },
-  #  ])
-    
-  #  Order.create(
-  #  [
-  #      {
-  #          user-id: '1',
-  #          token: 'approved-1234',
-  #          price: '18',
-  #          message: "I'm looking forward to see it happens!",
-  #      },
-  #      {
-  #          user-id: '1',
-  #          token: 'approved-ABCD',
-  #          price: '360',
-  #          message: "Have a good luck",
-  #      },
-  #  ])
